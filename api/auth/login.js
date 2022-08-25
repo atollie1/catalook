09028dc889bcef8950ee4faad65d5d7421c00009
Santo Blasius <santoblasius@atech.software>
2022-08-25 10:17:47 +0700

    configure to only accept POST request

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,43 +1,49 @@
 import crypto from 'crypto'
 
 export default function handler (req, res) {
-  const { email, password } = req.body
-  const knownUser = {
-    email: 'admin@mail.com',
-    password: 'itsSoSecret'
-  }
+  if (req.method === 'POST') {
+    const { email, password } = req.body
+    const knownUser = {
+      email: 'admin@mail.com',
+      password: 'itsSoSecret'
+    }
 
-  const correctEmail = email === knownUser.email
-  const correctPassword = password === knownUser.password
+    const correctEmail = email === knownUser.email
+    const correctPassword = password === knownUser.password
 
-  if (correctEmail && correctPassword) {
-    return res.status(200).json({
-      success: true,
-      data: {
-        token: crypto.randomBytes(32).toString('hex'),
-        user: knownUser
+    if (correctEmail && correctPassword) {
+      return res.status(200).json({
+        success: true,
+        data: {
+          token: crypto.randomBytes(32).toString('hex'),
+          user: knownUser
+        }
+      })
+    } else {
+      let errors = {}
+
+      if (!correctEmail) {
+        errors = {
+          ...errors,
+          email: 'Invalid email'
+        }
       }
-    })
+
+      if (!correctPassword) {
+        errors = {
+          ...errors,
+          password: 'Invalid password'
+        }
+      }
+
+      res.status(422).json({
+        success: false,
+        errors
+      })
+    }
   } else {
-    let errors = {}
-
-    if (!correctEmail) {
-      errors = {
-        ...errors,
-        email: 'Invalid email'
-      }
-    }
-
-    if (!correctPassword) {
-      errors = {
-        ...errors,
-        password: 'Invalid password'
-      }
-    }
-
-    res.status(422).json({
-      success: false,
-      errors
+    res.status(404).json({
+      message: 'no route found'
     })
   }
 }
